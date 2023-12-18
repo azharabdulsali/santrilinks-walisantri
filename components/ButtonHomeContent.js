97c52cff00c9ecd97af2026fb90978de1ba39364
mensafri <safrijawad1245@gmail.com>
2023-12-18 12:43:58 +0700

    fix minor

--- components/ButtonHomeContent.js
+++ components/ButtonHomeContent.js
@@ -1,44 +1,60 @@
 import { StyleSheet, Text, View, Pressable, Image } from "react-native";
 import { screenWidth, screenHeight } from "../constants/scale";
 
-export default ButtonHomeContent = ({ color, shadowColor, text, onPress }) => {
-    const styles = StyleSheet.create({
-        container: {
-            position: "relative", // Make the container relative for absolute positioning of shadow
-        },
-        button: {
-            backgroundColor: color,
-            width: screenWidth/3,
-            height: screenHeight/10,
-            borderRadius: 20,
-            display: "flex",
-            flexDirection: "row",
-            rowGap: 11,
-        },
-        shadow: {
-            backgroundColor: shadowColor,
-            width: screenWidth/3,
-            height: screenHeight/10,
-            borderRadius: 20,
-            position: "absolute",
-            zIndex: -1,
-            top: 8,
-            left: 9,
-        },
-    });
+export default function ButtonHomeContent({
+  color,
+  shadowColor,
+  text,
+  onPress,
+}) {
+  const styles = StyleSheet.create({
+    container: {
+      position: "relative", // Make the container relative for absolute positioning of shadow
+    },
+    button: {
+      backgroundColor: color,
+      width: screenWidth / 3,
+      height: screenHeight / 10,
+      borderRadius: 20,
+      display: "flex",
+      flexDirection: "row",
+      rowGap: 11,
+    },
+    shadow: {
+      backgroundColor: shadowColor,
+      width: screenWidth / 3,
+      height: screenHeight / 10,
+      borderRadius: 20,
+      position: "absolute",
+      zIndex: -1,
+      top: 8,
+      left: 9,
+    },
+  });
 
-    return (
-        <View style={styles.container}>
-            <Pressable style={styles.button} onPress={onPress}>
-                <View style={{ display: "flex", justifyContent: "center", marginLeft: screenWidth/(375/21) }}>
-                    <Image source={require("../assets/images/vector-alquran.png")} />
-                </View>
-                <View style={{ display: "flex", justifyContent: "center", width: screenWidth/6.7, marginLeft: screenWidth/(375/11), fontFamily: "Poppins_500Medium" }}>
-                    <Text style={{ fontSize: screenWidth/26.7 }}>{text}</Text>
-                    {/* <Text style={{ fontSize: 10 }}>{screenWidth}</Text> */}
-                </View>
-            </Pressable>
-            <View style={styles.shadow}></View>
+  return (
+    <View style={styles.container}>
+      <Pressable style={styles.button} onPress={onPress}>
+        <View
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginLeft: screenWidth / (375 / 21),
+          }}>
+          <Image source={require("../assets/images/vector-alquran.png")} />
         </View>
-    );
-};
+        <View
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            width: screenWidth / 6.7,
+            marginLeft: screenWidth / (375 / 11),
+            fontFamily: "Poppins_500Medium",
+          }}>
+          <Text style={{ fontSize: screenWidth / 26.7 }}>{text}</Text>
+        </View>
+      </Pressable>
+      <View style={styles.shadow}></View>
+    </View>
+  );
+}
--- screen/HomeScreen.js
+++ screen/HomeScreen.js
@@ -1,4 +1,11 @@
-import { View, Text, Pressable, StyleSheet, Image, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  Pressable,
+  StyleSheet,
+  Image,
+  ScrollView,
+} from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import ButtonHomeContent from "../components/ButtonHomeContent";
 import { Colors } from "../constants/colors";
@@ -10,8 +17,7 @@ export default function HomeScreen({ navigation }) {
       colors={["#46A175", "#75AA50"]}
       start={{ x: 0, y: 0 }}
       end={{ x: 1, y: 1 }}
-      style={{ flex: 1 }}
-    >
+      style={{ flex: 1 }}>
       <View style={styles.container}>
         <View style={styles.viewAtas}>
           <View style={styles.viewText1}>
@@ -23,8 +29,7 @@ export default function HomeScreen({ navigation }) {
           <View style={styles.lingkaran}></View>
           <Image
             source={require("../assets/images/foto-orang.png")}
-            style={styles.fotoOrang}
-          ></Image>
+            style={styles.fotoOrang}></Image>
         </View>
         <View style={styles.viewBawah}>
           <View style={{ flex: 1, flexDirection: "column", marginTop: 43 }}>
@@ -34,19 +39,22 @@ export default function HomeScreen({ navigation }) {
                 flexDirection: "row",
                 justifyContent: "space-evenly",
                 // alignItems: "center",
-              }}
-            >
-                <ButtonHomeContent
-                  color={Colors.greenBg}
-                  shadowColor={Colors.greenSecondary}
-                  text="Setor Hafalan"
-                  onPress={() => navigation.replace("Stack", { screen: "SetorHafalan" })}
-                />
+              }}>
+              <ButtonHomeContent
+                color={Colors.greenBg}
+                shadowColor={Colors.greenSecondary}
+                text="Setor Hafalan"
+                onPress={() =>
+                  navigation.navigate("Stack", { screen: "SetorHafalan" })
+                }
+              />
               <ButtonHomeContent
                 color={Colors.greenSecondary}
                 shadowColor={Colors.greenBg}
                 text="Absensi Santri"
-                onPress={() => navigation.replace("Stack", { screen: "AbsensiSantri" })}
+                onPress={() =>
+                  navigation.navigate("Stack", { screen: "AbsensiSantri" })
+                }
               />
             </View>
             <View
@@ -55,26 +63,29 @@ export default function HomeScreen({ navigation }) {
                 flexDirection: "row",
                 justifyContent: "space-evenly",
                 // alignItems: "center",
-              }}
-            >
+              }}>
               <ButtonHomeContent
                 color={Colors.greenSecondary}
                 shadowColor={Colors.greenBg}
                 text="Point Santri"
-                onPress={() => navigation.replace("Stack", { screen: "PointSantri" })}
+                onPress={() =>
+                  navigation.navigate("Stack", { screen: "PointSantri" })
+                }
               />
               <ButtonHomeContent
                 color={Colors.greenBg}
                 shadowColor={Colors.greenSecondary}
                 text="Data Santri"
-                onPress={() => navigation.replace("Stack", { screen: "DataSantri" })}
+                onPress={() =>
+                  navigation.navigate("Stack", { screen: "DataSantri" })
+                }
               />
             </View>
           </View>
           <View style={styles.carousel}>
-            <HaditsCarousel 
-            hadits="خَيْرُكُمْ مَنْ تَعَلَّمَ الْقُرْآنَ وَعَلَّمَهُ" 
-            terjemahan="Sebaik-baik kalian adalah orang yang belajar Al-Qur`an dan mengajarkannya." 
+            <HaditsCarousel
+              hadits="خَيْرُكُمْ مَنْ تَعَلَّمَ الْقُرْآنَ وَعَلَّمَهُ"
+              terjemahan="Sebaik-baik kalian adalah orang yang belajar Al-Qur`an dan mengajarkannya."
             />
             {/* Belum dibuat sambungan carousel nya masi sederhana */}
           </View>
@@ -87,8 +98,8 @@ export default function HomeScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: screenHeight/812/44,
-    paddingBottom: screenHeight/13,
+    paddingTop: screenHeight / 812 / 44,
+    paddingBottom: screenHeight / 13,
   },
   viewAtas: {
     flex: 1,
@@ -101,18 +112,18 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 20,
   },
   viewText1: {
-    width: screenWidth/(375/195),
-    marginLeft: screenWidth/(375/27),
-    marginTop: screenHeight/(812/23),
+    width: screenWidth / (375 / 195),
+    marginLeft: screenWidth / (375 / 27),
+    marginTop: screenHeight / (812 / 23),
   },
   text1: {
     fontFamily: "Poppins_500Medium",
-    fontSize: screenWidth/(375/20),
+    fontSize: screenWidth / (375 / 20),
     color: "#fff",
   },
   buttonTebakAyat: {
-    marginLeft: screenWidth/(375/27),
-    marginTop: screenHeight/(812/36),
+    marginLeft: screenWidth / (375 / 27),
+    marginTop: screenHeight / (812 / 36),
     backgroundColor: "#DEFFC9",
     shadowColor: "#666",
     shadowOffset: { width: 0, height: 4 },
@@ -135,16 +146,16 @@ const styles = StyleSheet.create({
     backgroundColor: "#59CD95",
     opacity: 0.12,
     position: "absolute",
-    left: screenWidth/(375/61),
-    top: screenHeight/(812/72),
+    left: screenWidth / (375 / 61),
+    top: screenHeight / (812 / 72),
     zIndex: 2,
   },
   fotoOrang: {
-    width: screenWidth/(375/194),
-    height: screenWidth/(375/194),
+    width: screenWidth / (375 / 194),
+    height: screenWidth / (375 / 194),
     position: "absolute",
-    right: screenWidth/(375/10),
-    top: screenHeight/(812/10),
+    right: screenWidth / (375 / 10),
+    top: screenHeight / (812 / 10),
     zIndex: 4,
   },
   carousel: {
@@ -153,7 +164,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     // alignItems: "center",
     // marginTop: screenHeight/(812/47),
-    paddingHorizontal: screenWidth/(375/37),
-    paddingBottom: screenHeight/(812/12),
-  }
+    paddingHorizontal: screenWidth / (375 / 37),
+    paddingBottom: screenHeight / (812 / 12),
+  },
 });
