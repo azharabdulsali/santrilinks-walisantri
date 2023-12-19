51dc0633e8c520631427ad361015a7b356d89d4d
kafitramarna <kafitra.marna@gmail.com>
2023-12-19 18:54:30 +0700

    fix: change navigation.replace to navigation.navigate

--- screen/DataSantri.js
+++ screen/DataSantri.js
@@ -4,7 +4,7 @@ export default function DataSantri({navigation}) {
   return (
     <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
       <Text>Setor Hafalan</Text>
-      <Pressable onPress={() => navigation.replace("Main", { screen: "Beranda" })}>
+      <Pressable onPress={() => navigation.navigate("Main", { screen: "Beranda" })}>
         <Text>Kembali</Text>
       </Pressable>
     </View>
