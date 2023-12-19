af21a7ec343d357a539c88d77f419c7ab12dc7fd
kafitramarna <kafitra.marna@gmail.com>
2023-12-19 22:49:11 +0700

    feat:add screen TebakAyat and TebakAyatScreen2

--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -3,15 +3,19 @@ import SetorHafalan from "../screen/SetorHafalan";
 import AbsensiSantri from "../screen/AbsensiSantri";
 import DataSantri from "../screen/DataSantri";
 import PointSantri from "../screen/PointSantri";
+import TebakAyat from "../screen/TebakAyat";
+import TebakAyatScreen2 from "../screen/TebakAyatScreen2";
 const Stack = createNativeStackNavigator();
 
 export default function StackNavigator() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="SetorHafalan" component={SetorHafalan} />
-      <Stack.Screen name="AbsensiSantri" component={AbsensiSantri} />
-      <Stack.Screen name="DataSantri" component={DataSantri} />
-      <Stack.Screen name="PointSantri" component={PointSantri} />
+      <Stack.Screen name="SetorHafalan" component={SetorHafalan} options={{ headerShown: false }}/>
+      <Stack.Screen name="AbsensiSantri" component={AbsensiSantri} options={{ headerShown: false }} />
+      <Stack.Screen name="DataSantri" component={DataSantri} options={{ headerShown: false }}/>
+      <Stack.Screen name="PointSantri" component={PointSantri} options={{ headerShown: false }}/>
+      <Stack.Screen name="TebakAyat" component={TebakAyat} options={{ headerShown: false }}/>
+      <Stack.Screen name="TebakAyatScreen2" component={TebakAyatScreen2}/>
     </Stack.Navigator>
   );
 }
