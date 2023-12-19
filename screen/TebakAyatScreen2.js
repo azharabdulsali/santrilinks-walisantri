bc31321016e47ca275a2d8fa64b63b7681e529a8
kafitramarna <kafitra.marna@gmail.com>
2023-12-19 22:51:13 +0700

    feat: add screen TebakAyatScreen2

---
+++ screen/TebakAyatScreen2.js
@@ -0,0 +1,9 @@
+import { Text, View,Pressable } from "react-native";
+
+export default function TebakAyatScreen2({navigation}) {
+  return (
+    <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
+      <Text>Tebak ayat lagi</Text>
+    </View>
+  );
+}
