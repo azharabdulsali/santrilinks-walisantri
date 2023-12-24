5ad271d4330c65ae9258cc9eac083c7a564c0d48
kafitramarna <kafitra.marna@gmail.com>
2023-12-24 13:58:33 +0700

    feat: add file ButtonMulai.js

--- components/ButtonMulai.js
+++ components/ButtonMulai.js
@@ -1,11 +1,31 @@
-import { Pressable,Text } from "react-native";
+import { Pressable, Text } from "react-native";
 import { screenWidth } from "../constants/scale";
 import { Colors } from "../constants/colors";
 
-export default function ButtonMulai({navigation, onPress}) {
-    return (
-            <Pressable onPress={onPress} style={{backgroundColor:Colors.greenPrimary,justifyContent:"center",alignItems:"center", padding:10, width:screenWidth/(375/146), borderRadius:screenWidth/(375/10)}}>
-                <Text style={{fontSize:screenWidth/(375/12),fontFamily:"Poppins_600SemiBold",color:"white"}}>Masuk</Text>
-            </Pressable>
-    );
+export default function ButtonMulai({ navigation, onPress, disabled = false }) {
+  return (
+    <Pressable
+      disabled={disabled}
+      onPress={onPress}
+      style={{
+        backgroundColor: Colors.greenPrimary,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 10,
+        width: screenWidth / (375 / 146),
+        borderRadius: screenWidth / (375 / 10),
+        opacity: disabled ? 0.5 : 1,
+      }}
+    >
+      <Text
+        style={{
+          fontSize: screenWidth / (375 / 12),
+          fontFamily: "Poppins_600SemiBold",
+          color: "white",
+        }}
+      >
+        Mulai
+      </Text>
+    </Pressable>
+  );
 }
