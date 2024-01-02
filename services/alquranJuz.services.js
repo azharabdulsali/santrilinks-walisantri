b5a45515028500c35e5842806904bb4bdb8733ac
kafitramarna <kafitra.marna@gmail.com>
2024-01-02 20:33:36 +0700

    feat: change limit from 10 to 11

--- services/alquranJuz.services.js
+++ services/alquranJuz.services.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getJuzQuran = async (juz, offset) => {
     try {
-      const response = await axios.get(`https://api.alquran.cloud/v1/juz/${juz}/quran-uthmani?offset=${offset}&limit=10`);
+      const response = await axios.get(`https://api.alquran.cloud/v1/juz/${juz}/quran-uthmani?offset=${offset}&limit=11`);
       return response.data;
     } catch (error) {
       console.error(error);
