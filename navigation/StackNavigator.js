a9fde389383bd4c36c274df4e699cc9f64a05dd4
kafitramarna <kafitra.marna@gmail.com>
2024-01-05 20:54:59 +0700

    feat : add new Stack.Screen

--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -6,6 +6,7 @@ import PointSantri from "../screen/PointSantri";
 import TebakAyat from "../screen/TebakAyat";
 import TebakAyatScreen2 from "../screen/TebakAyatScreen2";
 import TebakAyatHasil from "../screen/TebakAyatHasil";
+import SetorHafalan2 from "../screen/SetorHafalan2";
 const Stack = createNativeStackNavigator();
 
 export default function StackNavigator() {
@@ -17,7 +18,8 @@ export default function StackNavigator() {
       <Stack.Screen name="PointSantri" component={PointSantri} options={{ headerShown: false }}/>
       <Stack.Screen name="TebakAyat" component={TebakAyat} options={{ headerShown: false }}/>
       <Stack.Screen name="TebakAyatScreen2" component={TebakAyatScreen2} options={{ headerShown: false }}/>
-      <Stack.Screen name="TebakAyatHasil" component={TebakAyatHasil} options={{ headerShown: true }}/>
+      <Stack.Screen name="TebakAyatHasil" component={TebakAyatHasil} options={{ headerShown: false }}/>
+      <Stack.Screen name="SetorHafalan2" component={SetorHafalan2} options={{ headerShown: false }}/>
     </Stack.Navigator>
   );
 }
